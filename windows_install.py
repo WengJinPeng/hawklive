af33889c7cd9005ebfec6050f1c07ef71492a65e
Weng JinPeng <weng_jin_peng@outlook.com>
2026-09-09 18:40:22 +0800

    Register SYSTEM startup task using the supported XML schema

--- windows_install.py
+++ windows_install.py
@@ -85,7 +85,6 @@ def scheduled_task_xml(executable: Path, data_dir: Path) -> str:
   <Principals>
     <Principal id="System">
       <UserId>S-1-5-18</UserId>
-      <LogonType>ServiceAccount</LogonType>
       <RunLevel>HighestAvailable</RunLevel>
     </Principal>
   </Principals>
